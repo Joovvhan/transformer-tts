747cbfaebb6d8ccaa8ba9438d757079dc581d90b
Joowhan Song <joovvhan.sonq@gmail.com>
2020-07-20 13:49:49 +0000

    Added Waveglow inference script

--- run_waveglow_inference.sh
+++ run_waveglow_inference.sh
@@ -1,1 +1,1 @@
-python3 inference_waveglow.py -f <(ls mel_spectrograms/*/*/*.pt) -w waveglow_256channels_universal_v5.pt -o . --is_fp16 -s 0.6
+python waveglow_inference.py -f <(ls mel_spectrograms/*/*/*.pt) -w waveglow/waveglow_256channels_universal_v5.pt -o . --is_fp16 -s 0.6
--- waveglow_inference.py
+++ waveglow_inference.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('./waveglow/')
+sys.path.append('./waveglow/tacotron2/')
 
 # *****************************************************************************
 #  Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
